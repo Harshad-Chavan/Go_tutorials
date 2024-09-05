a0fb59e79fa92f2d7198f7c1c3c135fc9552ef16
harshad <harshadchavan141@gmail.com>
2024-09-05 20:46:29 +0530

    key concepts

--- app.go
+++ app.go
@@ -1,8 +1,23 @@
+// very go file must have a package at the start
+// code must be organised in packages
+// main specifies that this package will be the entry point of the application we are building
 package main
 
+// fmt is a built in package from which we use the print fucntion
 import "fmt"
 
+// must be named main. go will call and execute this funciton when program starts
 func main() {
 	fmt.Print("Hello World")
 
 }
+
+// one module consists of multiple packages
+// -go build- command enabales us to create a executable
+
+// intilly go deos not recognise the folder as a module for that we have to run command
+// go mod init <path or name>
+// post module creation we can use go build to create executable file
+// ./first-app
+
+// libraries are not executbales so they dont need to have a main fucntion
---
+++ go.mod
@@ -0,0 +1,3 @@
+module exmaple.com/first-app
+
+go 1.23.0
