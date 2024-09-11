3e81a030ba908e0e49f7d29c609598904f3227f3
harshad <harshadchavan141@gmail.com>
2024-09-11 23:07:21 +0530

    declaring and instantiating structs

---
+++ Structs/structs.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// defining a struct
+type user struct {
+	firstName string
+	lastName  string
+	birthDate string
+	createdAt time.Time
+}
+
+func main() {
+	userFirstName := getUserData("Please enter your first name: ")
+	userLastName := getUserData("Please enter your last name: ")
+	userBirthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+
+	// declaring the varibale as a User type
+	var appUser user
+
+	//instantiating the appUser struct
+	appUser = user{
+		firstName: userFirstName,
+		lastName:  userLastName,
+		birthDate: userBirthdate,
+		createdAt: time.Now(),
+	}
+
+	fmt.Println(appUser)
+
+	// alternate way of instantiating make sure the order is same
+	// appUser = user{
+	// 	userFirstName,
+	// 	userLastName,
+	// 	userBirthdate,
+	// 	time.Now(),
+	// }
+
+	//we can create an empty struct also
+	// var appUser_emp user = user{}
+	// fmt.Println(appUser_emp)
+
+	// ... do something awesome with that gathered data!
+
+	fmt.Println(userFirstName, userLastName, userBirthdate)
+}
+
+func getUserData(promptText string) string {
+	fmt.Print(promptText)
+	var value string
+	fmt.Scan(&value)
+	return value
+}
