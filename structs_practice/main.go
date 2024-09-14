0490499e0b717a99911c951f02006712292d1d67
harshad <harshadchavan141@gmail.com>
2024-09-14 17:38:45 +0530

    reading multi word strings using bufio

--- structs_practice/main.go
+++ structs_practice/main.go
@@ -1,24 +1,48 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 
 	"example.com/notes/note"
 )
 
 func getNoteData() (string, string) {
-	noteTitle := getUserInput("Enter your note Title:")
-	noteContent := getUserInput("Enter your note content:")
+	noteTitle := getUserInput("Enter your note Title")
+	noteContent := getUserInput("Enter your note content")
 
 	return noteTitle, noteContent
 
 }
 
 func getUserInput(prompt string) string {
-	var value string
-	fmt.Print(prompt)
-	fmt.Scanln(&value)
-	return value
+	// problem : the below code will not accept multi word strings
+	var text string
+	// fmt.Print(prompt)
+	// fmt.Scanln(&value)
+	// return value
+
+	fmt.Printf("%v : ", prompt)
+
+	// this creates a reader object that reads from the command line
+	reader := bufio.NewReader(os.Stdin)
+
+	// this tells the reader object to read till \n or enter
+	text, err := reader.ReadString('\n')
+
+	if err != nil {
+		return ""
+	}
+
+	// this will remove the \n as it is also included
+	text = strings.TrimSuffix(text, "\n")
+
+	// this will remove the \r as it is also included this is for windows as next line in \r\n
+	text = strings.TrimSuffix(text, "\r")
+
+	return text
 }
 
 func main() {
