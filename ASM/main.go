f974698f3cf7874b58af53208f9188461181ea55
harshad <harshadchavan141@gmail.com>
2024-11-27 21:16:55 +0530

    For loop over aaray and map

--- ASM/main.go
+++ ASM/main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+type floatMap map[string]float64
+
+func (m floatMap) outputFloatMap() {
+	fmt.Print(m)
+}
+
 func main() {
 
 	// we can tell go to create a bigger array behind the scenes
@@ -22,12 +28,28 @@ func main() {
 
 	// make can also be used to make maps
 	//takes only one argument in case of maps as we cannnot et empty slots 5 is the max capacity
-	courses := make(map[string]float64, 5)
+	courses := make(floatMap, 5)
 
 	courses["go"] = 4.5
 	courses["python"] = 4.7
 	courses["C++"] = 4.8
 
-	fmt.Println(courses)
+	// fmt.Println(courses)
+	courses.outputFloatMap()
+
+	//Looping over arrays, slices, Maps
+	// for i := 0; i < len(userNames); i++ {
+	// 	fmt.Println(userNames[i])
+
+	// }
+
+	// range always return 2 values
+	for index, values := range userNames {
+		println(index, values)
+	}
+
+	for key, values := range courses {
+		println(key, values)
+	}
 
 }
