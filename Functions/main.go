5c1a87190700bd41a9f5f07fb6ff00cc5b39f3d4
harshad <harshadchavan141@gmail.com>
2024-11-29 08:13:03 +0530

    closure basic

--- Functions/main.go
+++ Functions/main.go
@@ -5,12 +5,20 @@ import "fmt"
 func main() {
 	numbers := []int{1, 2, 3}
 
-	transformed := transformNumbers(&numbers, func(number int) int { return number * 2 })
+	//anaonymous funtions are fucntions that are defined just in time instead of being defined n advance
+	//transformed := transformNumbers(&numbers, func(number int) int { return number * 2 })
 
-	fmt.Println(transformed)
+	//creating diuble fucntion using factory pattern
+	double := createTransformerFunc(2)
+	triple := createTransformerFunc(3)
+
+	dtransformed := transformNumbers(&numbers, double)
+	fmt.Println(dtransformed)
+
+	ttransformed := transformNumbers(&numbers, triple)
+	fmt.Println(ttransformed)
+
 }
-
-//anaonymous funtions are fucntions that are defined just in time instead of being defined n advance
 
 func transformNumbers(numbers *[]int, transform func(int) int) []int {
 	dNumbers := []int{}
@@ -20,4 +28,11 @@ func transformNumbers(numbers *[]int, transform func(int) int) []int {
 	}
 
 	return dNumbers
+}
+
+// closure example
+// Go functions may be closures. A closure is a function value that references variables from outside its body.
+// The function may access and assign to the referenced variables; in this sense the function is "bound" to the variables.
+func createTransformerFunc(factor int) func(int) int {
+	return func(value int) int { return value * factor }
 }
