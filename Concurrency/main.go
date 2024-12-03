e4543f58a543b0baf0ea54d26349f806d2b167bd
harshad <harshadchavan141@gmail.com>
2024-12-03 20:37:58 +0530

    basic concurrency

---
+++ Concurrency/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func greet(phrase string) {
+	fmt.Println("Hello!", phrase)
+}
+
+func slowGreet(phrase string) {
+	time.Sleep(3 * time.Second) // simulate a slow, long-taking task
+	fmt.Println("Hello!", phrase)
+}
+
+// agg 'go' in front of the methods to make them run as go routines
+// but this will not give any ouptut
+func main() {
+	go greet("Nice to meet you!")
+	go greet("How are you?")
+	go slowGreet("How ... are ... you ...?")
+	go greet("I hope you're liking the course!")
+}
