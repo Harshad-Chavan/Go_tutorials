3b015d9efef65f9703fc7ad92764e7d2ce380d97
harshad <harshadchavan141@gmail.com>
2024-12-14 18:35:16 +0530

    generating JWT

---
+++ REST-API-PROJECT/utils/jwt.go
@@ -0,0 +1,23 @@
+package utils
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+const secretkey = "supersecret"
+
+func GenerateToken(email string, userId int64) (string, error) {
+
+	// Generating token and adding claims to the token
+	// setting an expire time
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"email":  email,
+		"userId": userId,
+		"expire": time.Now().Add(time.Hour * 2).Unix(),
+	})
+
+	// token is generated but we convert it into a string with a secret key
+	return token.SignedString(secretkey)
+}
